cbca9781df4293c2076743ed0dbff890b038c828
Willian Zatta Leite <107430993+WillianZatta2@users.noreply.github.com>
2024-04-04 22:01:45 -0300

    Add files via upload

---
+++ Agenda/Agenda/Contato.cs
@@ -0,0 +1,33 @@
+﻿class Contato
+{
+    public string ID { get; set; }
+    public string Nome { get; set; }
+    public string NumeroTelefone { get; set; }
+    public string Grupo { get; set; }
+    public string Anotacoes { get; set; }
+    public string Endereco { get; set; }
+    public string Aniversario { get; set; }
+    public string Email { get; set; }
+    public Contato( string nome, string numeroTelefone, string email, string grupo, string anotacoes, string endereco, string aniversario )
+
+
+    {
+
+
+       
+        Nome = nome;
+        NumeroTelefone = numeroTelefone;
+        Email = email;
+        Grupo = grupo;
+        Anotacoes = anotacoes;
+        Endereco = endereco;
+        Aniversario = aniversario;
+
+
+    }
+
+    public override string ToString()
+    {
+        return $"{Nome}: {NumeroTelefone}: {Grupo}: {Anotacoes}: {Endereco}: {Aniversario}: {Email}";
+    }
+}
