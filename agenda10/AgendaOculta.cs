a2651de6a60c7c8d21f600b517a5f7f47c693cbf
Willian Zatta Leite <107430993+WillianZatta2@users.noreply.github.com>
2024-04-15 15:39:29 -0300

    Add files via upload

---
+++ agenda10/AgendaOculta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public class AgendaOculta : AgendaTelefonica
+{
+    public string nomeArquivoOculto;
+
+    public AgendaOculta(string nomeArquivo) : base()
+    {
+        nomeArquivoOculto = nomeArquivo;
+        contatos = CarregarContatosDeJSON(GetCaminhoArquivo());
+        proximoId = contatos.Count > 0 ? contatos.Max(c => c.ID) + 1 : 1;
+    }
+
+    public override string GetCaminhoArquivo()
+    {
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        return Path.Combine(desktopPath, $"{nomeArquivoOculto}.json");
+    }
+}
